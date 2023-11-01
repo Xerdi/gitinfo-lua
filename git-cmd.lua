36b15dbb6b6524cb7e41a6cbe191816161f79770
Erik Nijenhuis <erik@xerdi.com>
2023-11-01 15:41:39 +0100

    Add license and headers

--- git-cmd.lua
+++ git-cmd.lua
@@ -1,4 +1,20 @@
--- Part of git-latex
+-- git-cmd.lua
+-- Copyright 2023 E. Nijenhuis
+--
+-- This work may be distributed and/or modified under the
+-- conditions of the LaTeX Project Public License, either version 1.3
+-- of this license or (at your option) any later version.
+-- The latest version of this license is in
+-- http://www.latex-project.org/lppl.txt
+-- and version 1.3 or later is part of all distributions of LaTeX
+-- version 2005/12/01 or later.
+--
+-- This work has the LPPL maintenance status ‘maintained’.
+--
+-- The Current Maintainer of this work is E. Nijenhuis.
+--
+-- This work consists of the files git.sty git-latex.pdf git-cmd.lua
+-- and git-latex.lua
 
 local api = {
     cwd = nil,
