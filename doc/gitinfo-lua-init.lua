d2a27836c7627f1f73a5bf8aa42943218b2736da
Erik Nijenhuis <erik@xerdi.com>
2024-04-11 16:09:33 +0200

    Update documentation

--- doc/gitinfo-lua-init.lua
+++ doc/gitinfo-lua-init.lua
@@ -1,6 +1,7 @@
--- Global texconfig should already be available when executed with lualtex
+-- Global texconfig should already be available when executed with lualatex
 local texconfig = texconfig or require('texconfig')
 
--- Use restricted shell_escape with git as only command
+-- Use restricted shell_escape with git as only command.
+-- Add others where needed, separated with a comma (no spaces in between)
 texconfig.shell_escape = 'p'
 texconfig.shell_escape_commands = 'git'
